91d6e566992bc6545bb386a167459a4ebda0b031
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-06-13 14:26:19 +0200

    Write basic test to verify setup

---
+++ tests/main.test.js
@@ -0,0 +1,14 @@
+import { it, expect, describe } from "vitest";
+
+// Create a Test Suite:
+// Name of the test does not matter
+// describe("group", () => {});
+
+// Create a Test Case
+// it("should", () => {});
+
+describe("group", () => {
+  it("should", () => {
+    expect(1).toBeTruthy();
+  });
+});
