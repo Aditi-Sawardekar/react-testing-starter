e22febfefaf8a71a59e019d0c17892356509ba5e
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-06-18 23:04:40 +0200

    Add basic tests for Greet component

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,25 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+// Test Suite
+describe("Greet", () => {
+  it("Should render Hello with the name when name is provided", () => {
+    render(<Greet name={"Adi"} />);
+    //screen.debug();
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/hello /i);
+  });
+
+  it("Should render login button when the name is not provided", () => {
+    render(<Greet />);
+    //screen.debug();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
